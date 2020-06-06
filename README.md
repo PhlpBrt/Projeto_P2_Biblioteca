62145226b82fb342386f85fe1242d43902c4e059
Philipe Brito <38106689+PhlpBrt@users.noreply.github.com>
2020-06-06 00:10:36 -0300

    Inicio

---
+++ README.md
@@ -0,0 +1,5 @@
+Projeto Biblioteca em Java Desktop
+
+Software para biblioteca em java, simples. O sistema contém somente as funcionalidades básicas, desenvolvido a fim de obter nota para a matéria de Técnicas avançadas de programação.
+
+Author: Philipe Brito
---
+++ src/view/Biblioteca.java
@@ -0,0 +1,17 @@
+
+package view;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+
+public class Biblioteca {
+
+  
+    public static void main(String[] args) throws SQLException, ParseException {
+
+        JFPrincipal principal = new JFPrincipal();
+        principal.setVisible(true);
+        
+    }
+    
+}
